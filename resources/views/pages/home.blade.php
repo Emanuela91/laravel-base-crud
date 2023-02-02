6669750256febe97db64bfeb59ef6b5d7ea9233b
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-02 17:10:32 +0100

    Bonus 1: Stampato a schermo il nome e i miracoli

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Tutti i Santi</h1>
+    <ul>
+        @foreach ($saints as $saint)
+            <li>{{$saint -> nome}} - Miracoli: {{$saint -> numero_miracoli}} </li>
+        @endforeach
+    </ul>
 @endsection
